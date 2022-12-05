12697be93b959310050dcccbb6fc481063199250
coding-knowjam <hanwoodww@gmail.com>
2022-12-05 18:34:37 +0900

    solved: BOJ-4375

--- src/boj/BOJ_4375.java
+++ src/boj/BOJ_4375.java
@@ -1,28 +1,45 @@
 package boj;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class BOJ_4375 {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        String line;
-        while(!(line= reader.readLine()).equals("")){
-            int N = Integer.parseInt(line);
-            long number = 1;
-            long count = 1;
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        Scanner scanner = new Scanner(System.in);
+//        String line;
+//        while(!(line= reader.readLine()).equals("")){
+        while (scanner.hasNextInt()) {
 
-            while (number % N != 0) {
-                number = number * 10 + 1;
-                count++;
+//            int N = Integer.parseInt(line);
+            int N = scanner.nextInt();
+            int number = 0;
+
+            for (int i = 1; ; i++) {
+                number = (number * 10) + 1;
+                number = number % N;
+                if (number == 0) {
+                    System.out.println(i);
+                    break;
+//                    writer.append(String.valueOf(i));
+//                    writer.newLine();
+//                    break;
+                }
             }
-            writer.append(String.valueOf(count));
-            writer.newLine();
+//            long count = 1;
+
+//            while (number % N != 0) {
+//                number = (number * 10) + 1;
+//                number = number % N;
+//                count++;
+//            }
+
         }
 
-        writer.flush();
-        reader.close();
-        writer.close();
+//        writer.flush();
+//        reader.close();
+//        writer.close();
     }
 
 }
